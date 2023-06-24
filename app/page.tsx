d00c5a52305df0965ceb41e2e8bede292a61a744
Aryan Deora <adeora@iu.edu>
2023-06-23 22:34:21 -0400

    Add CSR

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,17 @@ export default async function Home({ searchParams }: any) {
   const cityName = searchParams.city || "";
   const temperature = await getTemperature(cityName);
 
+  const handleSubmit = async (formData: FormData) => {
+    "use server";
+    const cityName = formData.get("cityName");
+    redirect("/?city=" + cityName);
+  };
+
   return (
     <>
       <h2>Weather</h2>
-      <form>
-        <input name="city" placeholder="City name" />
+      <form action={handleSubmit}>
+        <input name="cityName" placeholder="City name" />
         <button type="submit">Check Weather</button>
       </form>
       {temperature && (
