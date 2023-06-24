2686a7f0d29fc7c6adb6229cae590a509c0684f3
Aryan Deora <adeora@iu.edu>
2023-06-23 22:30:36 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,22 @@
 import { revalidatePath } from "next/cache";
 import { getTemperature } from "./actions";
 import Temperature from "./Weather";
+import { redirect } from "next/navigation";
 
 export default async function Home({ searchParams }: any) {
-  const cityName = searchParams.city || '';
+  const cityName = searchParams.city || "";
   const temperature = await getTemperature(cityName);
-
-  const handleSubmit = async (formData: FormData) => {
-    'use server';
-    const cityName = formData.get('cityName');
-    // how do i reload the server component with the proper city?
-  };
 
   return (
     <>
       <h2>Weather</h2>
-      <form action={handleSubmit}>
-        <input name="cityName" placeholder="City name" />
-        <button type="submit">
-          Check Weather
-        </button>
+      <form>
+        <input name="city" placeholder="City name" />
+        <button type="submit">Check Weather</button>
       </form>
-      {temperature && <Temperature temperature={temperature} cityName={cityName} />}
+      {temperature && (
+        <Temperature temperature={temperature} cityName={cityName} />
+      )}
     </>
   );
 }
